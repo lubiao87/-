c6a66cee5da11522de88f1dbd1056655ad6641d1
biaol <695769659@qq.com>
2019-05-12 16:52:35 +0800

    【删除文件】删除测试组件

--- src/router.js
+++ src/router.js
@@ -42,17 +42,6 @@ const routes = [
     path: "/login",
     name: "login",
     component: () => import("./components/login/login.vue")
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import("./views/About.vue"),
-    meta: {
-      auth: true // 这里设置，当前路由需要校验
-    }
   }
 ];
 const router = new Router({
--- src/views/page1.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="about">
-    <h2>This is an about page1</h2>
-    <h2>This is an about page1</h2>
-    <h2>This is an about page1</h2>
-    <h2>This is an about page1</h2>
-    <h2>This is an about page1</h2>
-  </div>
-</template>
